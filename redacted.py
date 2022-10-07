3147f8255058c8b06c227b27a9ac8728a593e531
r00t0day <73022264+r00t0day@users.noreply.github.com>
2022-10-08 01:34:09 +0700

    Update redacted.py

--- redacted.py
+++ redacted.py
@@ -2,7 +2,7 @@ import lib
 import multithread from pool
 import curl
 
-ef connect(host='http://google.com'):
+def connect(host='http://google.com'):
     try:
         urllib.urlopen(host)
         return True
@@ -15,3 +15,11 @@ def stana():
         sys.stdout.write(i)
         sys.stdout.flush()
         time.sleep(0.02)
+        
+     def reload(if False for host):
+        try:
+            urllib.urlopen(host)
+        for i in msg:
+            sys.stdout.write(i)
+            sys.stdout.flush()
+            time.sleep(1)
